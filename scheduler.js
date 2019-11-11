3be1c18e7f01a5023495df04f8e747cc9b0dfd33
lilychu3 <41395358+lilychu3@users.noreply.github.com>
2019-11-11 07:30:57 -0800

    chuyue and lily combine changes

---
+++ scheduler.js
@@ -0,0 +1,29 @@
+
+function toggleCheckOne(i) {
+	const metas = document.getElementsByTagName('meta');
+
+	if (metas[i].getAttribute('content') === '0') {
+		metas[i].setAttribute('content', '1');
+	} else {
+		metas[i].setAttribute('content', '0');
+	}
+
+	changeImage(metas[2].getAttribute('content'), metas[3].getAttribute('content'));
+}
+
+function changeImage(one, two) {
+	var src = 'img/register-saved.png'
+
+	if (one === '0' && two === '0') {
+		//console.log("both unchecked");
+		src = 'img/blank-calendar.png';
+	} else if (one === '1' && two === '0') {
+		//console.log("one checked, two unchecked");
+		src = 'img/register-schedule.png';
+	} else if (one === '0' && two === '1') {
+		//console.log("one unchecked, two checked");
+		src = 'img/saved-schedule.png';
+	}
+
+	document.getElementById('calendar').src = src;
+}
