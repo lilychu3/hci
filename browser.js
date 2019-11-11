6ea26d34190a5cf53546dec7fc8e84b175637f65
lilychu3 <41395358+lilychu3@users.noreply.github.com>
2019-11-10 18:10:57 -0800

    browser is now complete

---
+++ browser.js
@@ -0,0 +1,70 @@
+function changeStar() {
+	var something = $("#changeWhenClicked");
+	var somethingSRC = something.attr("src");
+	console.log(somethingSRC);
+
+    if (somethingSRC == "img/empty-star.png") {
+       something.attr("src", "img/filled-star.png");
+        console.log("message");
+    } else {
+       something.attr("src", "img/empty-star.png");
+       	console.log("hi");
+    }
+}
+
+function changeStar2() {
+	var something = $("#changeWhenClicked2");
+	var somethingSRC = something.attr("src");
+	console.log(somethingSRC);
+	
+    if (somethingSRC == "img/empty-star.png") {
+       something.attr("src", "img/filled-star.png");
+        console.log("message");
+    } else {
+       something.attr("src", "img/empty-star.png");
+       	console.log("hi");
+    }
+}
+
+function changeStar3() {
+	var something = $("#changeWhenClicked3");
+	var somethingSRC = something.attr("src");
+	console.log(somethingSRC);
+	
+    if (somethingSRC == "img/empty-star.png") {
+       something.attr("src", "img/filled-star.png");
+        console.log("message");
+    } else {
+       something.attr("src", "img/empty-star.png");
+       	console.log("hi");
+    }
+}
+
+function changeStar4() {
+	var something = $("#changeWhenClicked4");
+	var somethingSRC = something.attr("src");
+	console.log(somethingSRC);
+	
+    if (somethingSRC == "img/empty-star.png") {
+       something.attr("src", "img/filled-star.png");
+        console.log("message");
+    } else {
+       something.attr("src", "img/empty-star.png");
+       	console.log("hi");
+    }
+}
+
+function changeStar5() {
+	var something = $("#changeWhenClicked5");
+	var somethingSRC = something.attr("src");
+	console.log(somethingSRC);
+	
+    if (somethingSRC == "img/empty-star.png") {
+       something.attr("src", "img/filled-star.png");
+        console.log("message");
+    } else {
+       something.attr("src", "img/empty-star.png");
+       	console.log("hi");
+    }
+}
+
